e4155c1964f5abbfefd19e1a9417bc39476f81d1
arjunkrishna <arjun.krishna@huawei.com>
2021-07-19 14:15:36 -0400

    complete ArrayList c++ implementation

--- 1_arrays_and_strings/ArrayList.cpp
+++ 1_arrays_and_strings/ArrayList.cpp
@@ -1,24 +1,68 @@
+#include <memory>
 #include <stdio.h>
 
-
 class ArrayList {
-	public:
-		ArrayList(int size1) {
-			this->size = size1;
-			this->data = new int[size];
-		}
+public:
+  ArrayList(int size1) {
+    size = size1;
+    data.reset(new int[size]);
+  }
 
-		void print() {
-			printf("%d\n", this->size);
-		}
-	private:
-		int size;
-		int *data;
+  void print() {
+    for (int i = 0; i < length; i++) {
+      printf("%d, ", data[i]);
+    }
+    printf("\n%d used out of %d\n\n", length, size);
+  }
+
+  void push(int newVal) {
+    if (length >= size) {
+      doubleSize();
+    }
+    data[length] = newVal;
+    length++;
+  }
+
+private:
+  int size;
+  int length;
+  std::unique_ptr<int[]> data;
+
+  void doubleSize() {
+    int newSize = size * 2;
+    std::unique_ptr<int[]> newData(new int[newSize]);
+
+    for (int i = 0; i < length; i++) {
+      newData[i] = data[i];
+    }
+
+    printf("ArrayList memory limit reached\nDoubling size from %d to %d\n",
+           size, newSize);
+    data = std::move(newData);
+    size = newSize;
+  }
 };
 
 int main() {
-	ArrayList arr(10);
-	arr.print();
-	printf("Hello World!\n");
-	return 0;
+  ArrayList arr(10);
+  arr.print();
+
+  // fill up to limit
+  for (int i = 0; i < 10; i++) {
+    arr.push(i);
+    arr.print();
+  }
+
+  // fill past limit, so it doubles it's size
+  for (int i = 0; i < 10; i++) {
+    arr.push(i);
+    arr.print();
+  }
+
+  // fill past limit again, so it soubles size multiple times
+  for (int i = 0; i < 100; i++) {
+    arr.push(i);
+    arr.print();
+  }
+  return 0;
 }
